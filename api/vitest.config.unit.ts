d343f37c4415d845d87148935ad5a436fceab3b4
marialuiza <maria_luiza1598@hotmail.com>
2024-06-05 19:06:24 -0300

    adding test coverage report

--- api/vitest.config.unit.ts
+++ api/vitest.config.unit.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
     test: {
         include: ["./src/test/*.test.ts"],
+        coverage: {
+            provider: 'istanbul', // or 'v8'
+            reporter: ['text', 'json', 'html'],
+        },
     },
     resolve: {
         alias: {
